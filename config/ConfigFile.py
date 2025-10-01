9bf43edc4ec634ad6fc6ce465b6b4ef367708d19
Yatin Katyal <yatinece@gmail.com>
2025-10-01 03:50:23 +0000

    Push updated code

--- config/ConfigFile.py
+++ config/ConfigFile.py
@@ -2,10 +2,11 @@ class Config:
     def __init__(self):
         self.data_path = "./data/"
         self.path = "./data/tokenizer.json"
-        self.dataset = "wikitext"
+        self.dataset = "wikitext1"
         self.batch_size_token=1024
         self.download_data_key = dict()
         self.download_data_key["wikitext"]= ("wikitext", "wikitext-2-raw-v1")
+        self.download_data_key["wikitext1"]= ("wikitext", "wikitext-103-raw-v1")
         self.download_data_key["shakespeare"]= ("tiny_shakespeare", None)
         self.download_data_key["ptb"]= ("ptb_text_only", None)
         self.download_data_key["news"]= ("ag_news", None)
